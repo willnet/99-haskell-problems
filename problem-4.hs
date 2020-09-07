f5722008f765f5e3685cdadaffc21bba1513ba37
willnet <netwillnet@gmail.com>
2020-09-07 09:33:00 +0900

    Refactor with let

--- problem-4.hs
+++ problem-4.hs
@@ -1,5 +1,5 @@
 myLength :: [a] -> Int
-myLengthInner :: [a] -> Int -> Int
-myLengthInner (x:xs) n = myLengthInner xs n+1
-myLengthInner [] n = n
-myLength x =  myLengthInner x 0
+myLength x = 
+  let myLengthInner (x:xs) n = myLengthInner xs n+1
+      myLengthInner [] n = n
+  in myLengthInner x 0
